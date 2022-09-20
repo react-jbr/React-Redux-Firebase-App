6a3e4a114860e2662537fc059dd6885a2df0af33
jabeermk <jabeermk@gmail.com>
2022-09-20 20:38:21 +0530

    Adding e=redux and reducers

---
+++ projectapp/src/store/reducers/authReducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const initialState ={}
+const authReducer = (state=initialState,action) => {
+  return (
+    <div>authReducer</div>
+  )
+}
+
+export default authReducer
---
+++ projectapp/src/store/reducers/projectReducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const initialState ={}
+const projectReducer = (state=initialState,action) => {
+  return (
+    <div>authReducer</div>
+  )
+}
+
+export default projectReducer
